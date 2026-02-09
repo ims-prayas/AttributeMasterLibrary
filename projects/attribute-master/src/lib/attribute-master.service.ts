970f4f079c0ae1846db72b59b27e4c9bd972e6d3
leezaims <leeza@imssoftware.com.np>
2026-02-09 18:05:20 +0545

    #182097-Attribute-value-master

--- projects/attribute-master/src/lib/attribute-master.service.ts
+++ projects/attribute-master/src/lib/attribute-master.service.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject, Subject } from 'rxjs';
 import { ConfigService } from './config.service';
 import { DialogComponent } from './components/dialog/dialog.component';
 import { MatDialog } from '@angular/material/dialog';
+import { AttributeValueList } from './components/attribute-value-master/Attribute';
 
 export interface AttributeMaster {
   AttributeType: string;
@@ -38,6 +39,12 @@ export interface MappingDetails{
   AttributeListID: number;
 }
 
+export interface AttributeValueMaster {
+  AttributeType: string;
+  ApplyTo: string;
+  AttributeValues: AttributeValueList[];
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -56,6 +63,12 @@ export class AttributeMasterService {
     MappedFor: '',
     AttributesList: []
   }
+
+  attributeValueMasterObj: AttributeValueMaster = {
+    AttributeType: '',
+    ApplyTo: '',
+    AttributeValues: [],
+  };
 
   private tableDataSubject = new BehaviorSubject<AttributeDetails[]>([]);
   tableData$ = this.tableDataSubject.asObservable();
@@ -216,7 +229,7 @@ export class AttributeMasterService {
     this.tableDataSubject.next([]);
   }
 
-  getAttributeNames(attributeType: string, applyTo: string) {
+  getAttributeNames(attributeType: string, applyTo?: string) {
     return this.http.get<any>(`${this.apiUrl}/getListOnlyAttributeNames?attributeType=${attributeType}&applyTo=${applyTo}`);
   }
 
@@ -224,7 +237,7 @@ export class AttributeMasterService {
     return this.http.get<any>(`${this.apiUrl}/getParentAttributeInfo?attributeListID=${id}`);
   }
 
-  getAttributeValueList(attributeType: string, applyTo: string){
+  getAttributeValueList(attributeType: string, applyTo?: string){
     return this.http.get<any>(`${this.apiUrl}/getAttributeValues?attributeType=${attributeType}&applyTo=${applyTo}`);
   }
 
@@ -252,4 +265,21 @@ export class AttributeMasterService {
       }
     });
   }
+
+  saveAttributeValues(mode: string = 'add') {
+    const body = { 
+      mode: mode,
+      data: this.attributeValueMasterObj 
+    };
+    return this.http.post(`${this.apiUrl}/saveAttributeValues`, body);
+  }
+
+  clearAttributeValues() {
+    this.attributeValueMasterObj = {
+      AttributeType: '',
+      ApplyTo: '',
+      AttributeValues: [],
+    };
+  }
+
 }
--- projects/attribute-master/src/lib/components/attribute-value-master/attribute-value-master.component.ts
+++ projects/attribute-master/src/lib/components/attribute-value-master/attribute-value-master.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { Subject , of } from 'rxjs';
+import { switchMap, takeUntil } from 'rxjs/operators';
 import {
   AttributeMasterService,
   AttributeDetails,
@@ -34,10 +34,12 @@ export class AttributeValueMasterComponent implements OnInit,OnDestroy {
 
 
     isEdit: boolean = false;
-    ID!: number;
+    editingIndex: number = -1;
     private destroy$ = new Subject<void>();
+
     page: number = 1;
-    itemsPerPage: number = 10;
+    itemsPerPage: number = 7;
+    
 
     constructor(
       private fb: FormBuilder,
@@ -48,18 +50,45 @@ export class AttributeValueMasterComponent implements OnInit,OnDestroy {
       this.initForm();
       this.loadAttribute();
       
-      
+      this.attributeMasterService.saveRequested$.pipe(takeUntil(this.destroy$)).subscribe(() => {
+        this.onSave();
+      });
+
       this.attributeMasterService.resetForm$.pipe(takeUntil(this.destroy$)).subscribe(() => {
           this.attributeValueForm.reset();
+          this.attributeValueList = [];
+          this.hasParent = false;
           this.isEdit = false;
+          this.editingIndex = -1;
         });
 
       this.attributeValueForm.get('AttributeType')?.valueChanges.subscribe((type) => {
-        this.attributeMasterService.getApplyToList(type).subscribe((res: any) => {
-          if (res.status === 'ok') {
-            this.applysTo = res.result;
-          }
-        });
+        if(!type) {
+          return;
+        }
+        this.attributeValueForm.get('ApplyTo')?.setValue('');
+        this.resetFormToAddMode();
+
+        if(type == 'Transaction'){  
+          this.attributeValueForm.get('ApplyTo')?.setValue(null);
+          this.attributeMasterService.getAttributeValueList(type).subscribe((res: any) => {
+            if (res.status === 'ok') {
+              this.attributeValueList = res.result;
+            }
+          });
+          this.attributeMasterService.getAttributeNames(type).subscribe((res: any) => {
+            if (res.status === 'ok') {
+              this.attributeNames = res.result;
+            }
+          });
+        }
+        else{
+          this.attributeMasterService.getApplyToList(type).subscribe((res: any) => {
+            if (res.status === 'ok') {
+              this.applysTo = res.result;
+            }
+          });
+        }
       });
 
       this.attributeValueForm.get('ApplyTo')?.valueChanges.subscribe((apply) => {
@@ -73,26 +102,33 @@ export class AttributeValueMasterComponent implements OnInit,OnDestroy {
           this.attributeMasterService.getAttributeValueList(attrType,apply).subscribe((res: any) => {
             if (res.status === 'ok') {
               this.attributeValueList = res.result;
-              console.log(this.attributeValueList)
             }
           });
         }
       });
 
       this.attributeValueForm.get('AttributeName')?.valueChanges.subscribe((obj) => {
-        this.attributeMasterService.getParentAttributeInfo(obj.AttributeListId).subscribe((res: any) => {
+        if (!obj || !obj.AttributeListId) {
+          return; 
+        }
+        this.attributeMasterService?.getParentAttributeInfo(obj.AttributeListId).subscribe((res: any) => {
           if (res.status === 'ok') {
             this.ParentAttributeInfo = res?.result;
+            const parentAttributeControl = this.attributeValueForm.get('ParentAttributeValue');
             if(this.ParentAttributeInfo.HasParent){
               this.hasParent = this.ParentAttributeInfo.HasParent;
               this.parentName = this.ParentAttributeInfo.ParentName;
               this.parentValueList = this.ParentAttributeInfo.ParentValues;
+              parentAttributeControl?.setValidators([Validators.required]);
             }
             else {
               this.hasParent = false;
               this.parentName = '';
               this.parentValueList = [];
+              parentAttributeControl?.clearValidators();
+              parentAttributeControl?.setValue('');
             }
+            parentAttributeControl?.updateValueAndValidity();
           }
         });
       });
@@ -103,15 +139,14 @@ export class AttributeValueMasterComponent implements OnInit,OnDestroy {
       this.destroy$.complete();
     }
 
-  
     initForm() {
       this.attributeValueForm = this.fb.group({
         AttributeType: ['', Validators.required],
         ApplyTo: ['', Validators.required],
         AttributeName: ['', Validators.required],
-        // ParentAttribute: ['', Validators.required],
         AttributeValue:['', Validators.required],
-        ParentAttributeValue:  ['', Validators.required],
+        ParentAttributeValue:  [''],
+        AttributeValueId: [''],
       });
     }
   
@@ -121,51 +156,173 @@ export class AttributeValueMasterComponent implements OnInit,OnDestroy {
       });
     }
   
-    onSubmit() {
-      
-    }
-    editAttribute(i: number){
-      const item = this.attributeValueList[i];
-  
-       const selectedAttribute = this.attributeNames.find(
+    editAttribute(pageIndex: number) {
+      const realIndex = (this.page - 1) * this.itemsPerPage + pageIndex;
+      this.editingIndex = realIndex;
+      this.isEdit = true;
+      this.attributeValueForm.get('AttributeName')?.disable();
+      const item = this.attributeValueList[realIndex];
+
+      const selectedAttributeName = this.attributeNames.find(
         attr => attr.AttributeListId === item.AttributeListId
       );
-  
-      const selectedParentValue = this.parentValueList.find(
-        parent => parent.AttributeValueId === item.ParentAttributeValueId
-      );
-  
- 
+
+      if (!selectedAttributeName) return;
       this.attributeValueForm.patchValue({
-        AttributeName: selectedAttribute,
-        AttributeValue: item.AttributeValue,
-        ParentAttributeValue: selectedParentValue
+        AttributeName: selectedAttributeName,
+        AttributeValue: item.AttributeValue || '',
+        AttributeValueId: item.AttributeValueId,
       });
-  
-      this.deleteAttribute(i);
+      of(item.AttributeListId)
+        .pipe(
+          switchMap((attributeListId) =>
+            this.attributeMasterService.getParentAttributeInfo(attributeListId)
+          ),
+          takeUntil(this.destroy$)
+        )
+        .subscribe((res: any) => {
+          if (res.status !== 'ok') return;
+
+          this.ParentAttributeInfo = res.result;
+
+          const parentControl =
+            this.attributeValueForm.get('ParentAttributeValue');
+
+          if (this.ParentAttributeInfo.HasParent) {
+            this.hasParent = true;
+            this.parentName = this.ParentAttributeInfo.ParentName;
+            this.parentValueList = this.ParentAttributeInfo.ParentValues;
+            const selectedParentValue = this.parentValueList.find(
+              parent =>
+                parent.AttributeValueId === item.ParentAttributeValueId
+            );
+
+            parentControl?.setValidators([Validators.required]);
+
+            this.attributeValueForm.patchValue({
+              ParentAttributeValue: selectedParentValue || ''
+            });
+          } else {
+            this.hasParent = false;
+            this.parentName = '';
+            this.parentValueList = [];
+
+            parentControl?.clearValidators();
+            parentControl?.setValue('');
+          }
+
+          parentControl?.updateValueAndValidity();
+        });
     }
 
-    deleteAttribute(i: number){
-      this.attributeValueList.splice(i, 1);
+
+    deleteAttribute(pageIndex: number) {
+      const realIndex = (this.page - 1) * this.itemsPerPage + pageIndex;
+      const item = this.attributeValueList[realIndex];
+      if ( item.AttributeValueId != null && this.attributeValueList.some(x => x.ParentAttributeValueId === item.AttributeValueId)) {
+       this.attributeMasterService.openErrorDialog("Cannot delete as this entry is used as parent.");
+       return;
+      }
+      this.attributeValueList.splice(realIndex, 1); 
+      if (this.editingIndex === realIndex) {
+        this.resetFormToAddMode();
+      }
+      else if (this.editingIndex > realIndex) {
+        this.editingIndex--;
+      }
     }
-    AddAttribute(){
+
+    AddAttribute() {
       const formValue = this.attributeValueForm.getRawValue();
-      console.log(formValue);
+      const newItem = {
+        AttributeListId: formValue.AttributeName.AttributeListId,
+        AttributeName: formValue.AttributeName.attributeName,
+        AttributeValue: formValue.AttributeValue,
+        AttributeValueId: formValue.AttributeValueId,
+        ...(this.parentName && { ParentAttributeName: this.parentName }),
+        ...(formValue.ParentAttributeValue && {
+        ParentAttributeValueName: formValue.ParentAttributeValue.AttributeValue,
+        ParentAttributeValueId: formValue.ParentAttributeValue.AttributeValueId
+        })
+      };
       
-      this.attributeValueList.push({
-          AttributeListId: formValue.AttributeName.AttributeListId,
-          AttributeName: formValue.AttributeName.attributeName,
-          AttributeValue: formValue.AttributeValue,
-          ParentAttributeName: this.parentName,
-          ParentAttributeValueName: formValue.ParentAttributeValue.AttributeValue,
-          ParentAttributeValueId: formValue.ParentAttributeValue.AttributeValueId,
-      });
-      console.log(this.attributeValueList);
+      const isDuplicate = this.attributeValueList.some(
+          (item, index) => {
+          if (this.isEdit && this.editingIndex === index) return false;
+          return (
+            (item.AttributeValue === formValue.AttributeValue) && (item.AttributeListId === formValue.AttributeName.AttributeListId)
+          );
+        }
+      );
+
+      if (isDuplicate) {
+        this.attributeMasterService.openErrorDialog('The Attribute Name with this Attribute Value already exists!');
+        return;
+      }
+      if (this.isEdit && this.editingIndex >= 0) {
+        this.attributeValueList[this.editingIndex] = newItem;
+      } else {
+        this.attributeValueList.push(newItem);
+      }
+      this.resetFormToAddMode();
+    }
+    
+    resetFormToAddMode() {
+      this.editingIndex = -1;
+      this.isEdit = false;
+      this.hasParent = false;
+      
+      this.attributeValueForm.get('AttributeName')?.enable({ emitEvent: false });
 
       this.attributeValueForm.patchValue({
         AttributeName: '',
         AttributeValue: '',
         ParentAttributeValue: ''
       });
+    }
+
+    get pagedAttributeValueList(): AttributeValueList[] {
+      const start = (this.page - 1) * this.itemsPerPage;
+      return this.attributeValueList.slice(start, start + this.itemsPerPage);
+    }
+    
+    onSave() {
+      const headerControls = ['AttributeType', 'ApplyTo'];
+
+        for (const controlName of headerControls) {
+          const control = this.attributeValueForm.get(controlName);
+          if (control?.invalid) {
+            this.attributeMasterService.openErrorDialog('Please select Attribute Type and Apply To');
+            return;
+          }
+        }
+
+        if (this.attributeValueList.length === 0) {
+          this.attributeMasterService.openErrorDialog('Please add at least one Attribute Value');
+          return;
+        }
+
+      const formValue = this.attributeValueForm.getRawValue();
+
+      const payload = {
+        AttributeType: formValue.AttributeType,
+        ApplyTo: formValue.ApplyTo,
+        AttributeValues: this.attributeValueList
+      };
+      this.attributeMasterService.attributeValueMasterObj = payload;
+
+      this.attributeMasterService
+        .saveAttributeValues(this.isEdit ? 'edit' : 'add')
+        .subscribe({
+          next: (res: any) => {
+            if (res.status === 'ok') {
+              this.attributeMasterService.resetForm();
+              this.attributeMasterService.openSuccessDialog(res.result);
+            } else {
+              this.attributeMasterService.openErrorDialog(res.result || 'Save failed');
+            }
+          },
+          error: () => this.attributeMasterService.openErrorDialog('Failed Saving Attribute')
+        });
     }
 }
